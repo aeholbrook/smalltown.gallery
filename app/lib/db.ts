715e7cc280d8c436f04c50cae6dc6d49ff686e5d
root <root@gitlab-runner.localdomain>
2026-02-11 05:25:16 +0000

    Add POSTGRES_URL fallbacks for Prisma connection

--- app/lib/db.ts
+++ app/lib/db.ts
@@ -4,7 +4,12 @@
 import { PrismaPg } from '@prisma/adapter-pg'
 import { PrismaClient } from '@prisma/client'
 
-const connectionString = process.env.POSTGRES_PRISMA_URL || process.env.DATABASE_URL || ''
+const connectionString =
+  process.env.POSTGRES_PRISMA_URL ||
+  process.env.DATABASE_URL ||
+  process.env.POSTGRES_URL ||
+  process.env.POSTGRES_URL_NON_POOLING ||
+  ''
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined
