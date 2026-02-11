9afc32274e933ff4de7727fe09a446ff3c6e8e6d
root <root@gitlab-runner.localdomain>
2026-02-11 05:30:39 +0000

    Prefer DATABASE_URL over POSTGRES_PRISMA_URL

--- app/lib/db.ts
+++ app/lib/db.ts
@@ -5,9 +5,9 @@ import { PrismaPg } from '@prisma/adapter-pg'
 import { PrismaClient } from '@prisma/client'
 
 const connectionString =
-  process.env.POSTGRES_PRISMA_URL ||
   process.env.DATABASE_URL ||
   process.env.POSTGRES_URL ||
+  process.env.POSTGRES_PRISMA_URL ||
   process.env.POSTGRES_URL_NON_POOLING ||
   ''
 
